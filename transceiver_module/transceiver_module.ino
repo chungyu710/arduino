5a4932de9878bd3c2cc3cf32858590c535376dbc
Chung Yu <chungyu710@gmail.com>
2021-06-07 21:38:56 -0400

    init

---
+++ transceiver_module/transceiver_module.ino
@@ -0,0 +1,11 @@
+//interfacing NRF24L01 with SPI using ESP32
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
